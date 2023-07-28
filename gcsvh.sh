25d5552d42964c3ed1e59da3828a160d5015e850
Angeleena <angeleena@qburst.com>
2023-07-28 10:42:30 +0530

    Shell script for build triigers

---
+++ gcsvh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while IFS='=' read -r -d '' n v; do
+    printf "  %s: ''%s''\n" "$n" "$v" >> app.yaml
+done < <(env -0)
+
+sed -i '/^  HOSTNAME:/d' app.yaml 
+sed -i '/^  _:/d' app.yaml 
+sed -i '/^  BUILDER_OUTPUT:/d' app.yaml 
+sed -i '/^  PWD:/d' app.yaml 
+sed -i '/^  HOME:/d' app.yaml 
+sed -i '/^  SHLVL:/d' app.yaml 
+sed -i '/^  PATH:/d' app.yaml 
+sed -i '/^  XTERM:/d' app.yaml
+sed -i '/^  LS_COLORS:/d' app.yaml
+sed -i '/^  DEBIAN_FRONTEND:/d' app.yaml
